a12733a8f735e796e7e7a6c31eb250c935b2b516
Joshua Cook <joshua_cook@vrtx.com>
2024-08-30 14:34:25 -0400

    fix: ruff check and format

---
+++ spatialde/__init__.py
@@ -0,0 +1,6 @@
+"""Spatial and temporal differential gene expression test."""
+
+from .aeh import fit_patterns, spatial_patterns
+from .base import dyn_de, model_search, run
+
+__version__ = "1.1.3"
